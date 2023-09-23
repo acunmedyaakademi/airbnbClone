d47887d8d99367e3af98b6a2a2521062c8482171
Koray Demir <koraydemir@gmail.com>
2023-09-24 01:11:52 +0300

    Yeni BottomNavBar Fonksiyonu eklendi

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import { mainNavBar } from './assets/jsx/mainNavbar'
+import { mainNavBarBottom } from './assets/jsx/mainNavBarBottom'
 
 export function App() {
 
@@ -11,6 +12,7 @@ export function App() {
         <>
 
             {mainNavBar()}
+            {mainNavBarBottom()}
             <div className="container">
 
             </div>
