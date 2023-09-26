e1d2f81bcb442d36ab4ebb2461d333f562cf99a6
Koray Demir <koraydemir@gmail.com>
2023-09-26 22:21:58 +0300

    NavBar CSS düzenlemeleri ve ilanlar eklentisi

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import { mainNavBar } from './assets/jsx/mainNavbar'
 import { mainNavBarBottom } from './assets/jsx/mainNavBarBottom'
+import { createAdverts } from './assets/jsx/advert'
 
 export function App() {
 
@@ -10,13 +11,16 @@ export function App() {
 
     return (
         <>
-
-            {mainNavBar()}
-            {mainNavBarBottom()}
-            <div className="container">
-
+            <div className="navbar" style={{position:"fixed",top:"0",backgroundColor:"white"}}>
+                {mainNavBar()}
+                {mainNavBarBottom()}
             </div>
-            
+            <div className="container">
+                <div className="adverts" style={{marginTop:"200px"}}>
+                    {createAdverts()}
+                </div>
+                
+            </div>
         </>
         
     )
