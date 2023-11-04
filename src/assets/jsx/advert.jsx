6306130a1895956142215570f517c08e0b2af3ea
Koray Demir <koraydemirmc@gmail.com>
2023-11-04 22:36:32 +0300

    Yeni kaydırılabilir ilanlar -CSS Hatası Mevcut

--- src/assets/jsx/advert.jsx
+++ src/assets/jsx/advert.jsx
@@ -1,17 +1,32 @@
 import { StarFilled,HeartTwoTone } from '@ant-design/icons';
+import { Carousel } from 'antd';
 
 
 
 export const createAdverts = () => {
-
+    const onChange = (currentSlide) => {
+        console.log(currentSlide);
+      };
     return(
         <>
             
-            <div className="card">
-                <div style={{ zIndex: "1", position: "absolute", marginLeft: "285px", marginTop: "10px" }}>
-                    <HeartTwoTone twoToneColor="#eb2f96" style={{ fontSize: "22px" }} />
-                </div>
-                <img src="https://a0.muscache.com/im/pictures/6943ce46-c64f-43b4-824b-5551735befcc.jpg?im_w=720" alt="" />
+
+            <div className="card" >
+                <Carousel style={{width:"320px"}} afterChange={onChange}>
+                    <div>
+                        <img src="https://a0.muscache.com/im/pictures/6943ce46-c64f-43b4-824b-5551735befcc.jpg?im_w=720" alt="" />
+                    </div>
+                    <div>
+                        <img src="https://a0.muscache.com/im/pictures/6943ce46-c64f-43b4-824b-5551735befcc.jpg?im_w=720" alt="" />
+                    </div>
+                    <div>
+                        <img src="https://a0.muscache.com/im/pictures/6943ce46-c64f-43b4-824b-5551735befcc.jpg?im_w=720" alt="" />
+                    </div>
+                    <div>
+                        <img src="https://a0.muscache.com/im/pictures/6943ce46-c64f-43b4-824b-5551735befcc.jpg?im_w=720" alt="" />
+                    </div>
+                
+                </Carousel>
                 <div className="info">
                     <div className="title" style={{display:"flex",justifyContent:"space-between",alignItems:"center",gap:"8px"}}>
                         <p className="title" >
@@ -24,11 +39,23 @@ export const createAdverts = () => {
                     <h5 style={{color:"#1d1d1d"}}><strong>5500TL</strong> Gece</h5>
                 </div>
             </div>
-            <div className="card">
-                <div style={{ zIndex: "1", position: "absolute", marginLeft: "285px", marginTop: "10px" }}>
-                    <HeartTwoTone twoToneColor="#eb2f96" style={{position:"absolute", fontSize: "22px" }} />
-                </div>
-                <img src="https://a0.muscache.com/im/pictures/6943ce46-c64f-43b4-824b-5551735befcc.jpg?im_w=720" alt="" />
+  
+            <div className="card" >
+                <Carousel style={{width:"320px",zIndex:"1"}} afterChange={onChange}>
+                    <div style={{zIndex:"1"}}> 
+                        <img style={{zIndex:"1"}} src="https://a0.muscache.com/im/pictures/6943ce46-c64f-43b4-824b-5551735befcc.jpg?im_w=720" alt="" />
+                    </div>
+                    <div style={{zIndex:"1"}}>
+                        <img style={{zIndex:"1"}} src="https://a0.muscache.com/im/pictures/6943ce46-c64f-43b4-824b-5551735befcc.jpg?im_w=720" alt="" />
+                    </div>
+                    <div style={{zIndex:"1"}}>
+                        <img style={{zIndex:"1"}} src="https://a0.muscache.com/im/pictures/6943ce46-c64f-43b4-824b-5551735befcc.jpg?im_w=720" alt="" />
+                    </div>
+                    <div style={{zIndex:"1"}}>
+                        <img style={{zIndex:"1"}} src="https://a0.muscache.com/im/pictures/6943ce46-c64f-43b4-824b-5551735befcc.jpg?im_w=720" alt="" />
+                    </div>
+                
+                </Carousel>
                 <div className="info">
                     <div className="title" style={{display:"flex",justifyContent:"space-between",alignItems:"center",gap:"8px"}}>
                         <p className="title" >
@@ -41,11 +68,23 @@ export const createAdverts = () => {
                     <h5 style={{color:"#1d1d1d"}}><strong>5500TL</strong> Gece</h5>
                 </div>
             </div>
-            <div className="card">
-                <div style={{ zIndex: "1", position: "absolute", marginLeft: "285px", marginTop: "10px" }}>
-                    <HeartTwoTone twoToneColor="#eb2f96" style={{ fontSize: "22px" }} />
-                </div>
-                <img src="https://a0.muscache.com/im/pictures/6943ce46-c64f-43b4-824b-5551735befcc.jpg?im_w=720" alt="" />
+
+            <div className="card" >
+                <Carousel style={{width:"320px"}} afterChange={onChange}>
+                    <div>
+                        <img src="https://a0.muscache.com/im/pictures/6943ce46-c64f-43b4-824b-5551735befcc.jpg?im_w=720" alt="" />
+                    </div>
+                    <div>
+                        <img src="https://a0.muscache.com/im/pictures/6943ce46-c64f-43b4-824b-5551735befcc.jpg?im_w=720" alt="" />
+                    </div>
+                    <div>
+                        <img src="https://a0.muscache.com/im/pictures/6943ce46-c64f-43b4-824b-5551735befcc.jpg?im_w=720" alt="" />
+                    </div>
+                    <div>
+                        <img src="https://a0.muscache.com/im/pictures/6943ce46-c64f-43b4-824b-5551735befcc.jpg?im_w=720" alt="" />
+                    </div>
+                
+                </Carousel>
                 <div className="info">
                     <div className="title" style={{display:"flex",justifyContent:"space-between",alignItems:"center",gap:"8px"}}>
                         <p className="title" >
@@ -58,11 +97,23 @@ export const createAdverts = () => {
                     <h5 style={{color:"#1d1d1d"}}><strong>5500TL</strong> Gece</h5>
                 </div>
             </div>
-            <div className="card">
-                <div style={{ zIndex: "0", position: "absolute", marginLeft: "285px", marginTop: "10px" }}>
-                    <HeartTwoTone twoToneColor="#eb2f96" style={{fontSize: "22px" }} />
-                </div>
-                <img src="https://a0.muscache.com/im/pictures/6943ce46-c64f-43b4-824b-5551735befcc.jpg?im_w=720" alt="" />
+
+            <div className="card" >
+                <Carousel style={{width:"320px"}} afterChange={onChange}>
+                    <div>
+                        <img src="https://a0.muscache.com/im/pictures/6943ce46-c64f-43b4-824b-5551735befcc.jpg?im_w=720" alt="" />
+                    </div>
+                    <div>
+                        <img src="https://a0.muscache.com/im/pictures/6943ce46-c64f-43b4-824b-5551735befcc.jpg?im_w=720" alt="" />
+                    </div>
+                    <div>
+                        <img src="https://a0.muscache.com/im/pictures/6943ce46-c64f-43b4-824b-5551735befcc.jpg?im_w=720" alt="" />
+                    </div>
+                    <div>
+                        <img src="https://a0.muscache.com/im/pictures/6943ce46-c64f-43b4-824b-5551735befcc.jpg?im_w=720" alt="" />
+                    </div>
+                
+                </Carousel>
                 <div className="info">
                     <div className="title" style={{display:"flex",justifyContent:"space-between",alignItems:"center",gap:"8px"}}>
                         <p className="title" >
@@ -75,11 +126,23 @@ export const createAdverts = () => {
                     <h5 style={{color:"#1d1d1d"}}><strong>5500TL</strong> Gece</h5>
                 </div>
             </div>
-            <div className="card">
-                <div style={{ zIndex: "1", position: "absolute", marginLeft: "285px", marginTop: "10px" }}>
-                    <HeartTwoTone twoToneColor="#eb2f96" style={{ fontSize: "22px" }} />
-                </div>
-                <img src="https://a0.muscache.com/im/pictures/6943ce46-c64f-43b4-824b-5551735befcc.jpg?im_w=720" alt="" />
+
+            <div className="card" >
+                <Carousel style={{width:"320px",zIndex:"1"}} afterChange={onChange}>
+                    <div>
+                        <img src="https://a0.muscache.com/im/pictures/6943ce46-c64f-43b4-824b-5551735befcc.jpg?im_w=720" alt="" />
+                    </div>
+                    <div>
+                        <img src="https://a0.muscache.com/im/pictures/6943ce46-c64f-43b4-824b-5551735befcc.jpg?im_w=720" alt="" />
+                    </div>
+                    <div>
+                        <img src="https://a0.muscache.com/im/pictures/6943ce46-c64f-43b4-824b-5551735befcc.jpg?im_w=720" alt="" />
+                    </div>
+                    <div>
+                        <img src="https://a0.muscache.com/im/pictures/6943ce46-c64f-43b4-824b-5551735befcc.jpg?im_w=720" alt="" />
+                    </div>
+                
+                </Carousel>
                 <div className="info">
                     <div className="title" style={{display:"flex",justifyContent:"space-between",alignItems:"center",gap:"8px"}}>
                         <p className="title" >
@@ -92,9 +155,6 @@ export const createAdverts = () => {
                     <h5 style={{color:"#1d1d1d"}}><strong>5500TL</strong> Gece</h5>
                 </div>
             </div>
-            
-            
-            
         </>
     )
 }
